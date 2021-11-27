e0cf8b400a1503d29ff7dd535b3e38105a0e0e13
starjm0807@postech.ac.kr <starjm0807@postech.ac.kr>
2021-11-27 20:42:45 +0900

    create last half run script

---
+++ code/run-022.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+python train.py --gpu-id -1 \
+                --loss Proxy_Anchor \
+                --model resnet50_cgd \
+                --embedding-size 512 \
+                --batch-size 120 \
+                --lr 1e-4 \
+                --dataset cub \
+                --warm 5 \
+                --bn-freeze 1 \
+                --lr-decay-step 5 \
+                --gd_config mg
+                --experiment_name resnet50_cgd-mg
---
+++ code/run-026.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+python train.py --gpu-id -1 \
+                --loss Proxy_Anchor \
+                --model resnet101_cgd \
+                --embedding-size 512 \
+                --batch-size 120 \
+                --lr 1e-4 \
+                --dataset cub \
+                --warm 5 \
+                --bn-freeze 1 \
+                --lr-decay-step 5 \
+                --gd_config smg
+                --experiment_name resnet101_cgd-smg
