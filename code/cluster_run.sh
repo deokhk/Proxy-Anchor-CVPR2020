f329b791e85cbd5e6d21aadf43e512a5ef957e68
deokhk <deokhk@postech.ac.kr>
2021-11-27 20:58:17 +0900

    Added cluster_run script

---
+++ code/cluster_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH -J proxy_CGD # job name
+#SBATCH -o sbatch_output_log/output_%x_%j.out # standard output and error log
+#SBATCH -p titanxp # queue name or partiton name
+#SBATCH -t 72:00:00 # Run time (hh:mm:ss)
+#SBATCH  --gres=gpu:2
+#SBATCH  --nodes=1
+#SBATCH  --ntasks=1
+#SBATCH  --tasks-per-node=1
+
+srun -l /bin/hostname
+srun -l /bin/pwd
+srun -l /bin/date
+
+module purge 
+
+date
+
+sh run-001.sh
+
+date
