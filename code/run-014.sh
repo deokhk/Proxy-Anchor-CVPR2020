620710815151bb72e3498c265d456130a46432eb
deokhk <deokhk@postech.ac.kr>
2021-11-27 20:41:38 +0900

    DOne scripting GooogleNet and Inception-BN

---
+++ code/run-014.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+python train.py --gpu-id -1 \
+                --loss Proxy_Anchor \
+                --model bn_inception_cgd \
+                --embedding-size 512 \
+                --batch-size 180 \
+                --lr 1e-4 \
+                --dataset cub \
+                --warm 1 \
+                --bn-freeze 1 \
+                --lr-decay-step 10 \
+                --gd_config SG \ 
+                --experiment_name bn_inception_SG
